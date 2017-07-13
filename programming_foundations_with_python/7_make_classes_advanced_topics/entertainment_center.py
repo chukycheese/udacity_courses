653cd303d372a7446930f376c27b00639305cd2e
Kyoyoung Chu <chusinev@hotmail.com>
2017-07-13 19:00:24 +0900

    20170713 Lesson 7: Make classes - Advanced topics

---
+++ programming_foundations_with_python/7_make_classes_advanced_topics/entertainment_center.py
@@ -0,0 +1,29 @@
+# import fresh_tomatoes
+import media
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+# print(toy_story.storyline)
+
+avatar = media.Movie("Avater",
+                     "A marine on an alien planet",
+                     "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=aVdO-cx-McA")
+# print(avatar.storyline)
+
+# Add another instance: okja
+okja = media.Movie("Okja",
+                   "Meet Mija, a young girl who risks everything to prevent a powerful, multi-national company from kidnapping her best friend - a fascinating animal named Okja.",
+                   "http://t1.gstatic.com/images?q=tbn:ANd9GcSkeKv-OKoYw_T-ObRdOKEMfdTrQoJa6O4dLiOhca2PyD1ZkC5c",
+                   "https://www.youtube.com/watch?v=AjCebKn4iic")
+
+# print(okja.storyline)
+# okja.show_trailer()
+
+# 15. Coding the Movie Website
+
+# print(media.Movie.VALID_RATINGS)
+
+# class variable .__doc__
+print(media.Movie.__doc__)
